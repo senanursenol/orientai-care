3e7075243ec84d3b5b026f156d9929649fb358c1
gammm-tech <gamzeakemoglu@gmail.com>
2026-07-19 17:33:59 +0300

    Update query.py

--- services/mentes-ai-service/app/services/rag/query.py
+++ services/mentes-ai-service/app/services/rag/query.py
@@ -1,5 +1,5 @@
-from retriever_service import retrieve_context
-
+from services.retriever_service import retrieve_context
+from services.log_service import save_interaction
 patient_id = input("Hasta ID (Örn: P-1001): ").strip()
 question = input("Sorunuzu girin: ").strip()
 
@@ -17,9 +17,20 @@ print("\n Bulunan En Alakalı Sonuçlar:\n")
 if not results["documents"] or not results["documents"][0]:
     print("Bu hastaya ait eşleşen bir bilgi bulunamadı.")
 else:
+    response = ""
+
     for document, metadata in zip(results["documents"][0], results["metadatas"][0]):
         print(f" Bilgi Tipi: {metadata.get('type', 'Bilinmiyor')}")
         if "category" in metadata:
             print(f" Kategori: {metadata['category']}")
         print(f" Belge İçeriği:\n{document}")
         print("-" * 40)
+
+        response += document + "\n"
+
+    save_interaction(
+        patient_id=1,
+        user_input=question,
+        response=response,
+        input_type="text"
+    )
